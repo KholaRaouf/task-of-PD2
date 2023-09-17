519b2e2f79e1079561378103f0883a34bb70e459
KholaRaouf <145254378+KholaRaouf@users.noreply.github.com>
2023-09-17 01:46:59 -0700

    Add files via upload

---
+++ Task7/task7.cpp
@@ -0,0 +1,14 @@
+#include<iostr	 eam>
+using namespace std;
+main(){
+system("color 47");
+
+
+
+cout<<"      XXX   XX   XX  XXXX  XXXX      XXX   XXXXX  XXXXXX  "<<endl;
+cout<<"    XX XX  XXX  XX XX      XX      XX XX  XX  XX   XX     "<<endl;
+cout<<"   XX   XX XXXX XX XX      XX     XX   XX XX  XX   XX     "<<endl;
+cout<<"   XX   XX XX XXXX  XXXX   XX     XX   XX XXXXX    XX     "<<endl;
+cout<<"   XXXXXXX XX  XXX     XX  XX     XXXXXXX XX XX    XX     "<<endl; 
+cout<<"   XX   XX XX   XX     XX  XX     XX   XX XX  XX   XX     "<<endl;
+cout<<"   XX   XX XX   XX XXXXX  XXXX    XX   XX XX  XX   XX";}
